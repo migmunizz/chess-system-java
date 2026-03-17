824bcbfc7042fb80741ec5fe756ba154fcc5fa0e
Miguel Muniz <mr.muniz4230@gmail.com>
2026-03-17 19:29:03 -0300

    project criated

---
+++ src/aplication/Main.java
@@ -0,0 +1,11 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Hello world");
+
+    }}
